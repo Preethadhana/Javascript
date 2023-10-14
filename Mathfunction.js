b6900eaada27918fae3165792a2d3eceadfff86a
Preethadhana <preetha.r2012@gmail.com>
2023-10-14 20:15:00 +0530

    created javascript file

---
+++ Mathfunction.js
@@ -0,0 +1,14 @@
+var c=Math.PI;
+var c=Math.E;
+var c=Math.round(5.8);
+var c=Math.floor(5.58);
+var c=Math.ceil(5.58);
+var c=Math.sqrt(90);
+var c=Math.abs(-45);
+var c=Math.trunc(4.9);//Return Integer only
+var c=Math.pow(2,4);
+var c=Math.min(10,50,5,45,8);
+var c=Math.max(10,50,5,45,8);
+
+ 
+console.log(c)
---
+++ Objectkey.js
@@ -0,0 +1,18 @@
+let user = {
+    name: "Tutor Joes",
+    age: 35,
+    city: "Salem",
+    contact: "9043017689",
+  };
+
+let arr_keys=Object.keys(user);
+console.table(arr_keys);
+
+let arr_values=Object.values(user);
+console.table(arr_values);
+
+for(let i=0;i<arr_keys.length;i++)
+{
+  //console.log(arr_keys[i]+" : "+arr_values[i]);
+  console.log(user[arr_keys[i]]);
+}
